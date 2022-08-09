f94a5639b2c85b57f73a46d0bfaefbcbcc4958ee
glaucia86 <glaucia_lemos86@hotmail.com>
2022-08-09 20:28:02 -0300

    initial commit.

---
+++ BlazorApp_CRUD/BlazorApp_CRUD/Shared/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp_CRUD.Shared.Models
+{
+    public class User
+    {
+        public int Userid { get; set; }
+
+        public string Username { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phonenumber { get; set; }
+
+        public string Emailid { get; set; }
+    }
+}
