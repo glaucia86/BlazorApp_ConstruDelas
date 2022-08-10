db32d99fca3aaa47e260ad211736e12fb7407228
glaucia86 <glaucia_lemos86@hotmail.com>
2022-08-10 20:41:03 -0300

    desenvolvimento do arquivo 'UserController.cs'

---
+++ BlazorApp_CRUD/BlazorApp_CRUD/Server/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using BlazorApp_CRUD.Server.Interfaces;
+using BlazorApp_CRUD.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp_CRUD.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUser _IUser;
+        public UserController(IUser iUser)
+        {
+            _IUser = iUser;
+        }
+        [HttpGet]
+        public async Task<List<User>> Get()
+        {
+            return await Task.FromResult(_IUser.GetUserDetails());
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            User user = _IUser.GetUserData(id);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public void Post(User user)
+        {
+            _IUser.AddUser(user);
+        }
+        [HttpPut]
+        public void Put(User user)
+        {
+            _IUser.UpdateUserDetails(user);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _IUser.DeleteUser(id);
+            return Ok();
+        }
+    }
+}
