a4ed8baaa0eee018ec7e54203be968d986d371fe
glaucia86 <glaucia_lemos86@hotmail.com>
2022-08-09 21:03:37 -0300

    desenvolvimento da classe Interface 'IUser'

--- BlazorApp_CRUD/BlazorApp_CRUD/Server/Interfaces/IUser.cs
+++ BlazorApp_CRUD/BlazorApp_CRUD/Server/Interfaces/IUser.cs
@@ -1,6 +1,22 @@
-﻿namespace BlazorApp_CRUD.Server.Interfaces
+﻿using BlazorApp_CRUD.Shared.Models;
+
+namespace BlazorApp_CRUD.Server.Interfaces
 {
     public interface IUser
     {
+        // CRUD 
+        /*
+         C = Create (Criar): POST
+         R = Read (Ler): GET
+         U = Update (Atualizar) PUT
+         D = Delete (Deletar) Delete
+         */
+
+        public List<User> GetUserDetails();
+        public void AddUser(User user);
+        public void UpdateUserDetails(User user);
+        public void GetUserData(int id);
+        public void DeleteUser(int id);
+
     }
 }
